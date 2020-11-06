a564e2dabcfaa5c1b43232d315c4eabb2477a69f
VisheshPandita <vishesh.pndta@gmail.com>
2020-11-07 01:09:52 +0530

    event model

--- booking/event/models.py
+++ booking/event/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Event(models.Model):
+
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4, 
+        editable=False
+    )
+    name = models.CharField(
+        verbose_name='Event Name',
+        max_length=200
+    )
+    seats = models.IntegerField(default=0)
+    price = models.IntegerField(default=0)
+    discription = models.TextField(
+        verbose_name='About the event',
+        null=True,
+        blank=True
+    )
+    time_start = models.DateTimeField(verbose_name='Time of starting of event')
+    time_end = models.DateTimeField(verbose_name='Time of ending of event')
+    address1 = models.CharField(
+        "Address line 1",
+        max_length=1024,
+    )
+    address2 = models.CharField(
+        "Address line 2",
+        max_length=1024,
+    )
+    zip_code = models.CharField(
+        "ZIP / Postal code",
+        max_length=12,
+    )
+    city = models.CharField(
+        "City",
+        max_length=1024,
+    )
+    country = models.CharField(
+        "Country",
+        max_length=3,
+    )
